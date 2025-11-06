c7b53f80545d291898c54ca10d8ce7d0c2bd2206
น้องเค <57751959+pong34811@users.noreply.github.com>
2025-11-06 08:07:54 +0000

    Add script to disable web search features in Windows 10/11

---
+++ disble-search-bing.bat
@@ -0,0 +1,5 @@
+# ปิดการค้นหาผ่านเว็บใน Windows 10/11
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaEnabled" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "AllowSearchToUseLocation" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchHistoryEnabled" -Value 0
