c615c5d175af67620209a0823b5356c07fe1b40f
น้องเค <57751959+pong34811@users.noreply.github.com>
2025-11-07 17:20:26 +0700

    Update disble-search-bing.bat

--- disble-search-bing.bat
+++ disble-search-bing.bat
@@ -1,6 +1,9 @@
-# ปิดการค้นหาผ่านเว็บใน Windows 10/11
+#ปิด Bing Search และ Cortana ใน Windows Search
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaEnabled" -Value 0
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "AllowSearchToUseLocation" -Value 0
-# Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchHistoryEnabled" -Value 0
 
+# ปิดการค้นหาผ่าน Bing / Store จากช่องค้นหา
+New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Windows Search" -Name "ConnectedSearchUseWeb" -Value 0 -Force
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Windows Search" -Name "ConnectedSearchUseWebOverMeteredConnections" -Value 0 -Force
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Windows Search" -Name "AllowSearchToUseLocation" -Value 0 -Force
