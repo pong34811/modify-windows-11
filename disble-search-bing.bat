a2e6753b1e061bfa7c5f5d752a71cde00626efc0
น้องเค <57751959+pong34811@users.noreply.github.com>
2025-11-07 16:28:34 +0700

    Modify disble-search-bing.bat to retain search history
    Re-enable the SearchHistoryEnabled property in the script.

--- disble-search-bing.bat
+++ disble-search-bing.bat
@@ -2,4 +2,5 @@
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaEnabled" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "AllowSearchToUseLocation" -Value 0
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchHistoryEnabled" -Value 0
+# Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchHistoryEnabled" -Value 0
+
